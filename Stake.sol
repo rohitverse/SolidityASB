6fc3f50a63f919bc55ed8fe4b53dc80ac45fafee
rohitverse <rohitkr23072001@gmail.com>
2023-07-18 13:13:50 +0530

    Added Stake.sol

--- Stake.sol
+++ Stake.sol
@@ -7,24 +7,27 @@ contract Stake {
     IERC20 public token;
     uint public INTEREST = 5;
     uint public TAX = 1;
-    uint public minute = 60 seconds;
-    uint public day = 24 hours;
-    uint public reward;
-    uint public totalAmount;
+    uint minute = 60 seconds;
+    uint day = 24 hours;
+    uint reward;
+    uint totalAmount;
+    uint public amount;
     address public owner;
     mapping(address => uint) public userStake;
     mapping(address => uint) public stakeUserTimestamp;
 
     constructor(address _token) {
         token = IERC20(_token);
+        owner = msg.sender;
     }
 
-    function stake(uint amount) public returns (bool) {
+    function stake(uint _amount) public returns (bool) {
         require(userStake[msg.sender] == 0, "Can only stake once");
-        require(token.balanceOf(msg.sender) >= amount, "Insufficient Token");
-        token.transferFrom(msg.sender, address(this), amount);
+        require(token.balanceOf(msg.sender) >= _amount, "Insufficient Token");
+        token.transferFrom(msg.sender, address(this), _amount);
         stakeUserTimestamp[msg.sender] = block.timestamp;
-        userStake[msg.sender] = amount;
+        userStake[msg.sender] = _amount;
+        amount = _amount;
         return true;
     }
 
@@ -33,24 +36,24 @@ contract Stake {
         INTEREST = _interest;
     }
 
-    function unstake(uint amount) public returns (bool) {
+    function unstake() public returns (bool) {
         require(userStake[msg.sender] > 0, "No stake to unstake");
         require(userStake[msg.sender] == amount, "Unstake Correct Amount");
         require(
             (block.timestamp - stakeUserTimestamp[msg.sender]) >= day,
-            "Stake duration must be at least 1 minute"
+            "Stake duration must be at least 1 day"
         );
         uint numberOfDays = (block.timestamp - stakeUserTimestamp[msg.sender]) /
             day;
         reward = numberOfDays * INTEREST;
-        token.mint(msg.sender, reward);
+        token.mint(address(this), reward);
         totalAmount = (amount + reward) - TAX;
         require(
             token.balanceOf(address(this)) >= totalAmount,
             "Contract has insufficient balance"
         );
         token.transfer(msg.sender, totalAmount);
-        userStake[msg.sender] = 0;
+        delete userStake[msg.sender];
         return true;
     }
 
