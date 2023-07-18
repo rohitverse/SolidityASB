4abdc1cbcd7753d15eee44018cd7fc7bbf8b4be1
rohitverse <rohitkr23072001@gmail.com>
2023-07-18 13:15:43 +0530

    Fix ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -4,20 +4,20 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract ERC is ERC20{
+contract ERC is ERC20 {
     address public owner;
 
     constructor() ERC20("ROHIT", "RHT") {
         owner = _msgSender();
         _mint(msg.sender, 1000);
+        _mint(address(this), 1000);
     }
 
-    function mint(address newOwner,uint amount)public{
-            _mint(newOwner, amount);
-    }
-    function burn(uint amount)public{
-            _burn(msg.sender, amount);
+    function mint(address newOwner, uint amount) external {
+        _mint(newOwner, amount);
     }
 
-  
+    function burn(uint amount) public {
+        _burn(msg.sender, amount);
+    }
 }
